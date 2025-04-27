52a456f1e8b7e2db5986d7d68bdf5d35cd1cc186
Syed Muhammad Murtaza <smmhj19@gmail.com>
2025-04-27 16:19:04 +0500

    Brand Section Added

--- src/components/Section/Home-1/Brand/Brand.jsx
+++ src/components/Section/Home-1/Brand/Brand.jsx
@@ -7,33 +7,43 @@ const BrandSection = () => {
         <div className="row">
           <div className="col-lg-5">
             <div className="zubuz-brand-logo-content">
-              <h3>
-                Over 50,000 people rely on our app for their money choices
+              <img className="vbIcon" src="/images/v2/VisualStudio-Icon.png" alt=""/>
+              <h3 className="headingBrandSection">
+                Technologies We Use.
               </h3>
             </div>
           </div>
           <div className="col-lg-7">
             <Marquee speed="30" className="zubuz-brand-slider">
-              <div className="zubuz-brand-item">
-                <img src="/images/v1/b_1.png" alt="" />
+            <div className="zubuz-brand-item">
+                <img className="techLogo" src="/images/v2/Nextjs.png" alt="" />
               </div>
               <div className="zubuz-brand-item">
-                <img src="/images/v1/b_2.png" alt="" />
+                <img className="techLogo" src="/images/v2/Typescript.png" alt="" />
               </div>
               <div className="zubuz-brand-item">
-                <img src="/images/v1/b_3.png" alt="" />
+                <img className="techLogo" src="/images/v2/React.png" alt="" />
               </div>
               <div className="zubuz-brand-item">
-                <img src="/images/v1/b_4.png" alt="" />
+                <img className="techLogo" src="/images/v2/Javascript.png" alt="" />
               </div>
               <div className="zubuz-brand-item">
-                <img src="/images/v1/b_5.png" alt="" />
+                <img className="techLogo" src="/images/v2/Nodejs.png" alt="" />
               </div>
               <div className="zubuz-brand-item">
-                <img src="/images/v1/b_6.png" alt="" />
+                <img className="techLogo" src="/images/v2/Swift.png" alt="" />
+              </div>
+              <div className="zubuz-brand-item">
+                <img className="techLogo" src="/images/v2/Wordpress.png" alt="" />
+              </div>
+              <div className="zubuz-brand-item">
+                <img className="techLogo" src="/images/v2/Shopify.png" alt="" />
+              </div>
+              <div className="zubuz-brand-item">
+                <img className="techLogo" src="/images/v2/Amazon.png" alt="" />
               </div>
             </Marquee>
-            <Marquee
+            {/* <Marquee
               speed="30"
               direction="right"
               className="zubuz-brand-slider"
@@ -57,7 +67,7 @@ const BrandSection = () => {
               <div className="zubuz-brand-item">
                 <img src="/images/v1/b_6.png" alt="" />
               </div>
-            </Marquee>
+            </Marquee> */}
           </div>
         </div>
       </div>
