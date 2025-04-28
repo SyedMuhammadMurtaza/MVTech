a8b28be13590d2d8fb99c52d70faf7e21af10043
Syed Muhammad Murtaza <smmhj19@gmail.com>
2025-04-29 00:09:15 +0500

    Services Section Added

--- src/components/Section/Home-1/Content/ContentOne.jsx
+++ src/components/Section/Home-1/Content/ContentOne.jsx
@@ -1,41 +1,101 @@
 const ContentSectionOne = () => {
   return (
-    <div className="section zubuz-section-padding2 white-bg">
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-5">
-            <div className="zubuz-thumb thumb-pr">
-              <img src="/images/v1/mocup01.png" alt="" />
-              <div className="zubuz-thumb-card">
-                <img src="/images/v1/card1.png" alt="" />
+    <>
+      <div className="section zubuz-section-padding2 white-bg">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-5">
+              <div className="zubuz-thumb thumb-pr">
+                <img src="/images/v1/mockup-1.png" alt="" />
+                <div className="zubuz-thumb-card">
+                  <img src="/images/v1/card1.png" alt="" />
+                </div>
               </div>
             </div>
-          </div>
-          <div className="col-lg-7 d-flex align-items-center">
-            <div className="zubuz-default-content">
-              <h2>Easily monitor all financial activitie</h2>
-              <p>
-                Users can input their income & expenses, categorize
-                transactions, and view reports and summaries of their spending
-                habits.
-              </p>
-              <div className="zubuz-extara-mt">
-                <p>
-                  <span className="font-semibold">Budgeting:</span> It enables
-                  effective budgeting and expense tracking, helping you stay on
-                  top of your financial goals.
+            <div className="col-lg-7 d-flex align-items-center">
+              <div className="zubuz-default-content">
+                <h2 className="headingContentOne">Website & E-commerce Store Development</h2>
+                <p className="paraContentOne">
+                  Offering a comprehensive range of responsive and scalable solutions for websites and web apps.
                 </p>
-                <p>
-                  <span className="font-semibold">Expense Tracking:</span> Users can
-                  input their income & expenses, categorize transactions, and
-                  view reports and summaries of their spending habits.
-                </p>
+                <div className="zubuz-extara-mt">
+                  <div class="row">
+                    <div class="col-xl-4 col-md-6">
+                      <div class="zubuz-iconbox-wrap center">
+                        <div class="zubuz-iconbox-icon">
+                          <img src="/images/v1/icon3.png" alt="Financial Planning" />
+                        </div>
+                        <div class="zubuz-iconbox-data">
+                          <h3>Designing UI/UX</h3>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-xl-4 col-md-6">
+                      <div class="zubuz-iconbox-wrap center">
+                        <div class="zubuz-iconbox-icon">
+                          <img src="/images/v1/icon4.png" alt="Security Features" />
+                        </div>
+
+                        <div class="zubuz-iconbox-data">
+                          <h3>Frontend Development</h3>
+     
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-xl-4 col-md-6">
+                      <div class="zubuz-iconbox-wrap center">
+                        <div class="zubuz-iconbox-icon">
+                          <img src="/images/v1/icon5.png" alt="Tax Preparation" />
+                        </div>
+                        <div class="zubuz-iconbox-data">
+                          <h3>Backend Development</h3>
+                          
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-xl-4 col-md-6">
+                      <div class="zubuz-iconbox-wrap center">
+                        <div class="zubuz-iconbox-icon">
+                          <img src="/images/v1/icon6.png" alt="Offline Access" />
+                        </div>
+                        <div class="zubuz-iconbox-data">
+                          <h3>Analytics & Reporting</h3>
+                      
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-xl-4 col-md-6">
+                      <div class="zubuz-iconbox-wrap center">
+                        <div class="zubuz-iconbox-icon">
+                          <img src="/images/v1/icon7.png" alt="Currency Conversion" />
+                        </div>
+                        <div class="zubuz-iconbox-data">
+                          <h3>Hosting & Deployment</h3>
+                       
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-xl-4 col-md-6">
+                      <div class="zubuz-iconbox-wrap center">
+                        <div class="zubuz-iconbox-icon">
+                          <img src="/images/v1/icon8.png" alt="Integration Support" />
+                        </div>
+                        <div class="zubuz-iconbox-data">
+                          <h3>Maintenance & Support</h3>
+                       
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+
+
+    </>
   );
 };
 
--- src/components/Section/Home-1/Content/ContentTwo.jsx
+++ src/components/Section/Home-1/Content/ContentTwo.jsx
@@ -5,7 +5,7 @@ const ContentSectionTwo = () => {
         <div className="row">
           <div className="col-lg-5 order-lg-2">
             <div className="zubuz-thumb thumb-pl">
-              <img src="/images/v1/mocup2.png" alt="" />
+              <img src="/images/v1/Iphone-Mockup.png" alt="" />
               <div className="zubuz-thumb-card2">
                 <img src="/images/v1/card2.png" alt="" />
               </div>
@@ -13,38 +13,82 @@ const ContentSectionTwo = () => {
           </div>
           <div className="col-lg-7 d-flex align-items-center">
             <div className="zubuz-default-content">
-              <h2>Bank account & credit card linked</h2>
-              <p>
+              <h2 className="headingContentOne">Mobile App & Admin Panel Development</h2>
+              <p className="paraContentOne">
                 Easily link their bank accounts & credit cards to view account
                 balances, monitor transactions and receive alerts for unusual
                 activity.
               </p>
               <div className="zubuz-extara-mt">
-                <div className="zubuz-iconbox-wrap-left">
-                  <div className="zubuz-iconbox-icon">
-                    <img src="/images/v1/icon1.png" alt="" />
-                  </div>
-                  <div className="zubuz-iconbox-data">
-                    <span>Saving & Goal Setting</span>
-                    <p>
-                      Easily set money aside and build your savings for the
-                      future plan and track your financial goals.
-                    </p>
+                  <div class="row">
+                    <div class="col-xl-4 col-md-6">
+                      <div class="zubuz-iconbox-wrap center">
+                        <div class="zubuz-iconbox-icon">
+                          <img src="/images/v1/icon3.png" alt="Financial Planning" />
+                        </div>
+                        <div class="zubuz-iconbox-data">
+                          <h3> Strategic Requirement Analysis</h3>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-xl-4 col-md-6">
+                      <div class="zubuz-iconbox-wrap center">
+                        <div class="zubuz-iconbox-icon">
+                          <img src="/images/v1/icon4.png" alt="Security Features" />
+                        </div>
+
+                        <div class="zubuz-iconbox-data">
+                          <h3>UI/UX Designing</h3>
+     
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-xl-4 col-md-6">
+                      <div class="zubuz-iconbox-wrap center">
+                        <div class="zubuz-iconbox-icon">
+                          <img src="/images/v1/icon5.png" alt="Tax Preparation" />
+                        </div>
+                        <div class="zubuz-iconbox-data">
+                          <h3>Frontend / Backend Development</h3>
+                          
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-xl-4 col-md-6">
+                      <div class="zubuz-iconbox-wrap center">
+                        <div class="zubuz-iconbox-icon">
+                          <img src="/images/v1/icon6.png" alt="Offline Access" />
+                        </div>
+                        <div class="zubuz-iconbox-data">
+                          <h3>Testing & Optimization</h3>
+                      
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-xl-4 col-md-6">
+                      <div class="zubuz-iconbox-wrap center">
+                        <div class="zubuz-iconbox-icon">
+                          <img src="/images/v1/icon7.png" alt="Currency Conversion" />
+                        </div>
+                        <div class="zubuz-iconbox-data">
+                          <h3>Hosting & Deployment</h3>
+                       
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-xl-4 col-md-6">
+                      <div class="zubuz-iconbox-wrap center">
+                        <div class="zubuz-iconbox-icon">
+                          <img src="/images/v1/icon8.png" alt="Integration Support" />
+                        </div>
+                        <div class="zubuz-iconbox-data">
+                          <h3>Scalling & Updates</h3>
+                       
+                        </div>
+                      </div>
+                    </div>
                   </div>
                 </div>
-                <div className="zubuz-iconbox-wrap-left">
-                  <div className="zubuz-iconbox-icon">
-                    <img src="/images/v1/icon2.png" alt="" />
-                  </div>
-                  <div className="zubuz-iconbox-data">
-                    <span>Credit Score Monitoring</span>
-                    <p>
-                      Our finance app provides credit score monitoring to help
-                      users monitor their credit health.
-                    </p>
-                  </div>
-                </div>
-              </div>
             </div>
           </div>
         </div>
